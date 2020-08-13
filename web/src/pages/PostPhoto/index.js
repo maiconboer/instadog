acbe0827e7129c3cca7a6f54d9df197db4647e0d
Maicon Boer <maicon_boer@hotmail.com>
2020-08-13 02:57:52 -0300

    validando campos de upload de imagem

--- web/src/pages/PostPhoto/index.js
+++ web/src/pages/PostPhoto/index.js
@@ -7,41 +7,52 @@ import useForm from '../../hooks/useForm';
 import UserHeader from '../../components/UserHeader';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import Error from '../../components/Error';
 
 import { Container } from './styles.js';
 
 const PostPhoto = () => {
   const { data } = React.useContext(UserContext);
   const [image, setImage] = React.useState({})
-  const [dataForUpload, setDataForUpload] = React.useState({});
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
 
   const description = useForm();
   
-
   async function handleSubmit(event){
     event.preventDefault()
 
-    const formData = new FormData();
-    formData.append('file', image.raw);
-    formData.append('description', description.value);
-    formData.append('id', data.id);
+    try {
+      setError(null);
+      setLoading(true);
 
-    const token = window.localStorage.getItem('@dog:token');
+      if(image.raw === undefined) {  
+        setError('Anexar imagem')
+        return 
+      }
 
-    // const body = {};
+      const formData = new FormData();
+      formData.append('file', image.raw);
+      formData.append('description', description.value);
+      formData.append('id', data.id);
 
-    for (var pair of formData.entries()) {
-      console.log(pair[0]+ ', ' + pair[1]); 
-    }
+      const token = window.localStorage.getItem('@dog:token');
 
-    if(token) {  
-      const response = await api.post('/photos', formData, {
-        headers: {
-          Authorization: `Bearer ${token}`
+      if(token) {  
+        const response = await api.post('/photos', formData, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        if(response.status === 201) {
+          console.log('Sucesso no upload, redirecionar usuario') 
         }
-      });
+      }
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -71,12 +82,18 @@ const PostPhoto = () => {
             name='image_url' 
             id='image_url' 
             className='file'
+            
             onChange={handleImageUpload}
           />
 
-          <Button>Enviar</Button>
-        </form>
+          {error && <Error error={error} />}
 
+          {loading 
+           ? <Button disabled>Enviando...</Button>
+           : <Button>Enviar</Button>
+          }
+        </form>
+        
         <div>
           {image.preview && 
             <div 
